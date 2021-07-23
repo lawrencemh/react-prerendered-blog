cfe9a137fe24342f9c5a8ecf2c35aa4b724319b8
Lawrence <lawrencemh@outlook.com>
2021-07-23 11:47:03 +0100

    Add .env.example and update blogConfig use envs.

--- src/blogConfig.js
+++ src/blogConfig.js
@@ -2,26 +2,26 @@ export default {
     /**
      * The name of your blog.
      */
-    name: 'My Blog',
+    name: process.env.REACT_APP_BLOG_NAME || 'My Blog',
 
     /**
      * The image location of your logo if available.
      *
      * e.g. /images/logo.png
      */
-    logo: '/images/static/example-logo.svg',
+    logo: process.env.REACT_APP_LOGO_SRC || '/images/static/example-logo.svg',
 
     /**
      * The optional header external link. This will show on the header if setup
      */
     externalLink: {
         // If false will hide
-        show: true,
+        show: !!process.env.REACT_APP_SHOW_EXTERNAL_LINK,
 
         // The URL the user should be taken to
-        href: 'http://google.com',
+        href: process.env.REACT_APP_EXTERNAL_LINK_HREF || '#',
 
         // The CTA to display on the button
-        cta: 'More',
+        cta: process.env.REACT_APP_EXTERNAL_LINK_TEXT || 'Link',
     },
 };
